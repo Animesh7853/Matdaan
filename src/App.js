7d28ad58341a59544a8784c2d4ec5445dea52936
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-02-11 23:07:10 +0530

    forrm added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import UserOption from './components/useroption';
 import Login from './components/Login';
+import Form from './components/form';
+
+
+
 function App() {
   return (
     <>
 {/* <Login/> */}
- <UserOption/>
+ {/* <UserOption/> */}
+ <Form/>
  </>
   );
 }
