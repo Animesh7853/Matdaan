81650cf06d88de750ca916a60c700976e5a1b45b
Animesh Jha <jhaanimesh1903@gmail.com>
2024-03-16 21:44:06 +0530

    backend

---
+++ Backend/middleware/authentication.js
@@ -0,0 +1,23 @@
+const { validateToken } = require('../services/authentication');
+
+
+function checkForAuthenticationCookie(cookieName) {
+    return (req, res, next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+        if (!tokenCookieValue) {
+            return next(); // Move next() inside if block
+        }
+        try {
+            const userPayload = validateToken(tokenCookieValue);
+            req.user = userPayload;
+        } catch (error) {
+            // Handle token validation error here if needed
+            console.error('Error validating token:', error);
+        }
+        next(); // Call next() here after setting req.user
+    };
+}
+
+module.exports = {
+    checkForAuthenticationCookie,
+};
---
+++ Backend/routes/voter.js
@@ -0,0 +1,51 @@
+const {Router}= require("express");
+const Voter=require('../models/voter')
+
+const router = Router();
+
+router.get("/signup",(req,res)=>{
+    return res.status(201).json({msg:"pending"});
+});
+
+router.post('/signup', async (req, res) => {
+    const { firstName, lastName, email, aadharNumber, mobileNumber, password } = req.body;
+    try {
+        const newVoter = await Voter.create({
+            firstName,
+            lastName,
+            email,
+            aadharNumber,
+            mobileNumber,
+            password,
+        });
+        return res.status(201).json({ msg: "success", voter: newVoter });
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
+    }
+});
+
+router.get("/signin",(req,res)=>{
+    return res.status(201).json({ msg: "success" });
+});
+
+router.post("/signin",async(req,res)=>{
+    const {email,password}=req.body;
+    try {
+        
+        const token =await Voter.matchPasswordAndGenerateToken(email,password);
+        return res.cookie("token",token).status(201).json({ msg: "success" });
+    } catch (error) {
+        return res.status(301).json({
+            error:"Incorrect Email or Password",
+        });
+    }
+
+});
+
+router.get('/logout',(req,res)=>{
+    res.clearCookie("token").redirect("/signin");
+});
+
+
+
+module.exports=router;
