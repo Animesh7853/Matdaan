a82cf1c1b38c17a4f40c74ffc2fec9cdfa6e5201
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-04-06 23:40:55 +0530

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
-    <div>
-      This is Home
+    <>
+     <div className="container-fluid">
+    <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span className="fs-5 d-none d-sm-inline">Menu</span>
+                </a>
+                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link align-middle px-0">
+                            <FontAwesomeIcon icon = {faHome}/> <span className="ms-1 d-none d-sm-inline">Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Elections</span> </a>
+                        <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">UpComing Elections</span> </a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">OnGoing Elections</span> </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline"> Election Results</span></a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline"> Give Feedback</span></a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline"> Read News</span></a>
+                    </li>
+                   
+                    
+                   
+                </ul>
+                <hr/>
+                <div className="dropdown pb-4">
+                    <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle"/>
+                        <span className="d-none d-sm-inline mx-1">loser</span>
+                    </a>
+                    <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+  
+                        <li><a className="dropdown-item" href="#">Settings</a></li>
+                        <li><a className="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr className="dropdown-divider"/>
+                        </li>
+                        <li><a className="dropdown-item" href="#">Log out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="col py-3">
+            Content area...
+        </div>
     </div>
-  )
+</div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
