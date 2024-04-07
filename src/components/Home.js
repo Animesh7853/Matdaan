6bd08023cc5c2dcc65713b731c6e061e6919b0f3
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-04-07 18:57:12 +0530

    d

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ function Home() {
                     </li>
                     <li>
                         <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Elections</span> </a>
+                            <i className="fs-4 bi-speedometer2"></i> <FontAwesomeIcon icon = "faUsers"/><span className="ms-1 d-none d-sm-inline">Elections</span> </a>
                         <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                             <li className="w-100">
                                 <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">UpComing Elections</span> </a>
